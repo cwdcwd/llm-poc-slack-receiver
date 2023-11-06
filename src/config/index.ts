07fedab0ef76ceb9d94f2397147ee0c7aa6e7aa9
CWD <cwd@lazybaer.com>
2023-11-06 13:00:36 -0500

    I hate dotenv

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-import { config } from 'dotenv'
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` })
+// import { config } from 'dotenv'
+// config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` })
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true'
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, SLACK_SIGNING_SECRET } = process.env
