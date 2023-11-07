a4a487d487dcf95cb4d1fa2a9aca4fcb53b56532
CWD <cwd@lazybaer.com>
2023-11-07 15:36:04 -0500

    those options were deprecated

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,14 +3,12 @@ import { NODE_ENV, DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } from '@
 
 export const dbConnection = async () => {
   const dbConfig = {
-    url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/`,
+    url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
     options: {
       // auth: {
       //   username: DB_USER,
       //   password: DB_PASSWORD,
       // },
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     },
   }
 
