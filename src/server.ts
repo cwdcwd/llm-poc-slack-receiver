d53143bef38279d3be4858788d58b421010e2508
CWD <cwd@lazybaer.com>
2023-11-07 23:51:35 -0500

    I do no like all these weird environmental helper mods

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import { AuthRoute } from '@routes/auth.route'
 import { UserRoute } from '@routes/users.route'
 import { SlackRoute } from './routes/slacks.route'
 import { ValidateEnv } from '@utils/validateEnv'
+import { logger } from '@utils/logger'
 
-ValidateEnv()
+logger.info('starting up...')
+// ValidateEnv()
 
 const app = new App([new SlackRoute(), new UserRoute(), new AuthRoute()])
 
 app.listen()
+logger.info('started')
