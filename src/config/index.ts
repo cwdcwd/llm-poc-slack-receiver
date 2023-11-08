4ef14fb7fe84325eab1801bdff68436538b3400a
CWD <cwd@lazybaer.com>
2023-11-07 23:09:28 -0500

    I do not understand why it is crashing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,3 @@
-// import { config } from 'dotenv'
-// config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` })
-
 export const CREDENTIALS = process.env.CREDENTIALS === 'true'
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, SLACK_SIGNING_SECRET } = process.env
 export const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } = process.env
