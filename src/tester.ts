c06707d9a0c88544b2f6ed31c87d9ff604ce698e
CWD <cwd@lazybaer.com>
2023-11-10 11:02:07 -0500

    bring in existing route mods

--- src/tester.ts
+++ src/tester.ts
@@ -4,10 +4,19 @@ const port = process.env.PORT || 3000
 
 import { Routes } from '@interfaces/routes.interface'
 import { logger } from '@utils/logger'
+import { AuthRoute } from '@routes/auth.route'
+import { UserRoute } from '@routes/users.route'
+import { SlackRoute } from './routes/slacks.route'
 
 app.get('/', (req, res) => {
   logger.info('Hello World!')
   res.send('Hello World!')
+})
+
+const routes: Routes[] = [new SlackRoute(), new UserRoute(), new AuthRoute()]
+
+routes.forEach(route => {
+  app.use('/', route.router)
 })
 
 app.listen(port, () => {
