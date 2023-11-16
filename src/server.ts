b0e83f19bdb55fae33dffb663dfb9beaa987b1df
CWD <cwd@lazybaer.com>
2023-11-16 11:46:11 -0500

    add slack route back in. I suspect it's still a db connection issue

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import { App } from '@/app'
-// import { SlackRoute } from '@/routes/slacks.route'
+import { SlackRoute } from '@/routes/slacks.route'
 import { HealthRoute } from './routes/health.route'
 import { logger } from '@utils/logger'
 
 logger.info('starting up...')
 // ValidateEnv()
 
-const app = new App([new HealthRoute()])
+const app = new App([new HealthRoute(), new SlackRoute()])
 
 app.listen()
 logger.info('started')
